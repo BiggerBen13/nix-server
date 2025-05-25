9e857f990e507bab17ba3727b3c5f5c4a7847919
Ben Kirschniak <ben@kirschniak.de>
2025-05-25 20:48:54 +0200

    dev: fix

--- hosts/seele.nix
+++ hosts/seele.nix
@@ -3,12 +3,12 @@
         system = "x86_64-linux";
         specialArgs = inputs;
         modules = [
+            inputs.disko.nixosModules.disko
+            inputs.nix-minecraft.nixosModules.minecraft-servers
+        ] ++ [
             ../system/disk-configuration.nix
             ../system/sshd.nix
             ../host-configurations/seele.nix
             ../system/minecraft-server.nix
-        ] ++ [
-            inputs.disko.nixosModules.disko
-            inputs.nix-minecraft.nixosModules.minecraft-servers
         ];
 }
