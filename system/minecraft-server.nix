543e9b681c9b17ba9727c94f6604544413c031df
Ben Kirschniak <ben@kirschniak.de>
2025-05-25 21:06:23 +0200

    dev: fix

--- system/minecraft-server.nix
+++ system/minecraft-server.nix
@@ -1,20 +1,20 @@
 {pkgs, lib, ...}@inputs : 
 let 
-    modpack = pkgs.fetchPackwizModpack {
+    statech = pkgs.fetchPackwizModpack {
         url = "https://raw.githubusercontent.com/TheStaticVoid/StaTech-Industry/d0fc315c67f3eba1f720676b977e5d1bca9f00f0/pack.toml";
-        packHash = "";
+        packHash = "sha256-d0fc315c67f3eba1f720676b977e5d1bca9f00f0";
     };
 in
 {
-    services.minecraft-servers = {
+    services.minecraft-servers.servers.statech = {
         enable = true;
         eula = true;
         package = pkgs.fabricServers.fabric-1_19_4-0_16_14;
         files = {
-            "mods" = "${modpack}/mods";
-            "kubejs" = "${modpack}/kubejs";
-            "config" = "${modpack}/config";
-            "defaultconfigs" = "${modpack}/defaultconfigs";
+            "mods" = "${statech}/mods";
+            "kubejs" = "${statech}/kubejs";
+            "config" = "${statech}/config";
+            "defaultconfigs" = "${statech}/defaultconfigs";
         };
     };
 }
