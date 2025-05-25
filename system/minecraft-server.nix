7a9e77c54ce85ead337d72d8587ca0960268fae5
Ben Kirschniak <ben@kirschniak.de>
2025-05-25 21:03:36 +0200

    dev: fix

--- system/minecraft-server.nix
+++ system/minecraft-server.nix
@@ -2,14 +2,15 @@
 let 
     modpack = pkgs.fetchPackwizModpack {
         url = "https://raw.githubusercontent.com/TheStaticVoid/StaTech-Industry/d0fc315c67f3eba1f720676b977e5d1bca9f00f0/pack.toml";
-        packHash = "sha256-d0fc315c67f3eba1f720676b977e5d1bca9f00f0";
+        packHash = "";
     };
 in
 {
     services.minecraft-servers = {
         enable = true;
         eula = true;
-        symlinks = {
+        package = pkgs.fabricServers.fabric-1_19_4-0_16_14;
+        files = {
             "mods" = "${modpack}/mods";
             "kubejs" = "${modpack}/kubejs";
             "config" = "${modpack}/config";
