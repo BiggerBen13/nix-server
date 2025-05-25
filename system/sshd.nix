4530b1da6632456a21e2c51f6e0e50c6734c85e0
Ben Kirschniak <ben@kirschniak.de>
2025-05-25 15:37:36 +0200

    dev: fix

--- system/sshd.nix
+++ system/sshd.nix
@@ -3,11 +3,9 @@
         enable = true;
         startWhenNeeded = true;
         ports = [ 28 ];
-        authorizedKeys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8aSoHPcKExpy2WC+sEhGWnaHTxbydB3vPLtxBM+qa0 biggerben@powerbookpro"
-        ];
         settings = {
             PasswordAuthentication = false;
         };
     };
+    boot.initrd.network.ssh.authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8aSoHPcKExpy2WC+sEhGWnaHTxbydB3vPLtxBM+qa0 biggerben@powerbookpro" ];
 }
